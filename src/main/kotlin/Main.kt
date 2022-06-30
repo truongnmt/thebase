e9fc065aba903f832661f59618eea7efb0ae34c3
TruongNM <truongnmt.dev@gmail.com>
2022-06-30 14:19:13 +0900

    Create Parser from files

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,8 @@
 fun main(args: Array<String>) {
+//    var aliexCrawler = AliexCrawler("4001328236329")
+//    aliexCrawler.crawl()
 
+    val parser = Parser("A1091")
+    parser.parse()
+    parser.print()
 }
---
+++ src/main/kotlin/Parser.kt
@@ -0,0 +1,89 @@
+import java.awt.Color
+import java.awt.Font
+import java.awt.Graphics
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
+
+class Parser(val productId: String) {
+
+    val variants: MutableMap<Int, String> = mutableMapOf()
+    val mainImg = arrayListOf<String>()
+    var videoUrl = ""
+    var productVariants = arrayListOf<String>()
+
+    private val baseUrl = "/Users/stronglong/home/business/techtoy/"
+
+    private fun stampStyleNumberToImage(styleNo: Int, imageName: String): String {
+        val horizontalSpacing = 24
+        val verticalSpacing = 36
+        val image: BufferedImage = ImageIO.read(File("${baseUrl}${productId}/${imageName}"))
+        val g: Graphics = image.graphics
+
+        g.font = Font("Helvetica", 0, 70)
+        g.color = Color(241, 151, 55)
+        val styleNumberToString = "M${styleNo.toString().padStart(2, '0')}"
+        g.drawString(styleNumberToString, horizontalSpacing, image.height - verticalSpacing)
+        g.dispose()
+        val newImageName = "${styleNumberToString}${imageName}".replace("variants", "")
+        ImageIO.write(image, "jpg", File("${baseUrl}${productId}/${newImageName}"))
+
+        return newImageName
+    }
+
+    fun parse() {
+        var countVariant = 1
+        File("${baseUrl}${productId}/").walk().maxDepth(1).forEach {
+            if ("_variants-" in it.name) {
+                variants[countVariant] = stampStyleNumberToImage(countVariant, it.name)
+                countVariant += 1
+            }
+            if ("_main-" in it.name) {
+                mainImg.add(it.name)
+            }
+            if (".mp4" in it.name) {
+                videoUrl = it.name
+            }
+        }
+
+//        println("Variants:")
+//        for((k, v) in variants) {
+//            println("Variant ${k}: ${v}")
+//        }
+//        println("\nMain img:")
+//        mainImg.forEach{ println(it) }
+//        if (videoUrl != "") println("\nVideo url:\n${videoUrl}")
+
+        val dataTxtUrl = "${baseUrl}${productId}/data.txt"
+        var countLine = 1
+        var productIdentifier: String
+        var productTitle: String
+        var productPrice: String
+        File(dataTxtUrl).forEachLine {
+            when (countLine) {
+                1 -> productIdentifier = it
+                2 -> productTitle = it
+                3 -> productPrice = it
+                else -> {
+                    productVariants.add(it.replace("For iphone", "iPhone"))
+                }
+            }
+            countLine++
+        }
+//        println("\nProduct variants text:")
+//        productVariants.forEach { println(it) }
+    }
+
+    fun print() {
+        println("Variants:")
+        for((k, v) in variants) {
+            println("Variant ${k}: ${v}")
+        }
+        println("\nMain img:")
+        mainImg.forEach{ println(it) }
+        if (videoUrl != "") println("\nVideo url:\n${videoUrl}")
+
+        println("\nProduct variants text:")
+        productVariants.forEach { println(it) }
+    }
+}
